bb65fd3930f5a019514bcb20189199d6651e3f85
Monkey Balls <37158637+unclevito2017@users.noreply.github.com>
2023-12-14 21:50:09 -0500

    Added hash160 logic

---
+++ quantum-run-kyoto.py
@@ -0,0 +1,87 @@
+import hashlib
+import qiskit
+from qiskit import execute, Aer, IBMQ
+from qiskit.circuit.library import PhaseOracle
+from qiskit_ibm_provider import IBMProvider
+from qiskit import QuantumCircuit
+
+def sha256_compression_function(qc, message_bits, expression):
+    # Ensure the length of message_bits is 256
+    assert len(message_bits) == 256, "Message must be 256 bits long"
+
+    # Apply controlled-X gates based on the message bits
+    for i, bit in enumerate(message_bits[:32]):
+        if bit == '1' and i < 16:  # Ensure i is within the valid range
+            qc.cx(i, 31)  # Apply CX gate to qubit 16 with control qubit i
+
+    # Manually construct the boolean conditions from the expression
+    for i, char in enumerate(expression):
+        if char == '1' and i < 16:  # Ensure i is within the valid range
+            qc.x(i)  # Apply X gate to qubit i
+
+    # Apply the oracle to each qubit individually
+    for i in range(16):
+        qc.cx(i, 16)  # Controlled-X gate with control qubit i and target qubit 16
+        qc.x(i)  # Reset the control qubit qubit 16
+
+    # Apply the oracle for target_hash160 (assumed little-endian)
+    for i, bit in enumerate(target_hash160_binary):
+        if bit == '1':
+            qc.x(i)
+
+def main():
+    # Load IBM Quantum account
+    IBMQ.load_account()
+    provider = IBMProvider()  # No hub, group, or project parameters
+
+    target_address_hex = "20d45a6a762535700ce9e0b216e31994335db8a5"
+    target_address_decimal = int(target_address_hex, 16)
+
+    target_hash160_binary = bin(int(target_address_hex, 16))[2:].zfill(256)
+
+    start_range = 0x20000000000000000
+    end_range = 0x3ffffffffffffffff
+
+    # Iterate over the range
+    for decimal_value in range(int(start_range), int(end_range) + 1):
+        # Convert decimal value to bytes and binary string
+        hex_value = hex(decimal_value)[2:].zfill(32)  # Ensure a fixed length of 32 characters
+        message_bytes = bytes.fromhex(hex_value)
+        binary_message = ''.join(format(byte, '08b') for byte in message_bytes)
+        binary_message = binary_message.zfill(256)  # Pad to 256 bits
+
+        sha256_1 = hashlib.sha256(message_bytes).digest()
+        sha256_2 = hashlib.sha256(sha256_1).digest()
+        ripemd160 = hashlib.new('ripemd160')
+        ripemd160.update(sha256_2)
+        hash160 = ripemd160.digest()
+
+        qc = QuantumCircuit(32, 32, name="qc", global_phase=0)
+
+        for i, bit in enumerate(binary_message[:16]):
+            if bit == '1':
+                qc.x(i)
+
+        qc.measure_all()
+
+        backend = provider.get_backend('ibm_kyoto')
+
+        job = execute(qc, backend=backend, shots=1024)
+
+        # Get the results and extract the final state
+        counts = job.result().get_counts(qc)
+        final_state = int(list(counts.keys())[0].replace(" ", ""), 2)
+
+        # Check if the generated hash matches the target address characters 10
+        if hex(final_state)[:10] == hex(target_address_decimal)[:10]:
+            print(f"Target address found!")
+            print(f"Decimal Value: {decimal_value}")
+            print(f"Simulated Bitcoin hash160: {hex(final_state)[2:].zfill(40)}")
+            break
+
+    else:
+        print("Target address not found within the specified range.")
+
+if __name__ == "__main__":
+    main()
+
